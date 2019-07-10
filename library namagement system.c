92bc01a7f1ce6cf472f182234e00bdf56a42a7ce
Md,Mahbub Alam <39045006+Mahbub96@users.noreply.github.com>
2019-07-10 18:26:10 +0630

    version:0.01.03
    beta version

--- library namagement system.c
+++ library namagement system.c
@@ -1,3 +1,19 @@
+ /*************************************************************************
+   *  Project: $(proejct 01)
+   *  Project Name: $(Library Management System)
+   *  Function:Welcome,menu,add,list,search,modify,Removes;
+   *  c features:string,loop,file,structure,user define function
+   *************************************************************************
+   *  $Author: Mahbub Alam And Rakiba Akter $
+   *  $Name  : $ Library Management System
+   *  CSE 69-B 2019
+   *************************************************************************
+   *
+   *  Copyright 2019 by Anonymous Coder
+   *
+   ************************************************************************/
+
+
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
@@ -6,16 +22,18 @@
 #include<windows.h>
 
 struct books{
-    int serial;
+
+    char number[3];
     char name[50];
+    char Name[50];
     char writer[50];
     char department[30];
-    char pas[30];
-    char self;
+    //char pas[30];
+    char self[5];
 
 }b;
 void gotoxy(int x,int y);
-void pass();
+//void pass();
 void welcome();
 void menu();
 void add();
@@ -28,7 +46,8 @@ int main(){
     printf("*********Welcome to Library Management system**********\n\n\n\t Name:Mahbub Alam \t Stamford University Bangladesh ");
     Sleep(500);
     system("color e");
-    pass();
+   // pass();
+    menu();
 
     return 0;
 }
@@ -58,6 +77,9 @@ void welcome(){
     }
 
 }
+
+//Without password
+/*
 void pass(){
     system("cls");
     welcome();
@@ -94,7 +116,7 @@ void pass(){
     }
     fclose(fpr);
 }
-
+*/
 
 
 void menu(){
@@ -152,95 +174,248 @@ void menu(){
 }
 
 void add(){
+    system("cls");
+    welcome();
     FILE *fp,*fpr;
-    int serial=0;
     fp=fopen("data.dat","ab+");
+
     if(!fp){
         fp=fopen("data.dat","wb+");
         if(!fp){
-            printf("\n\n\n\tSystem error!\a\a\a");
-        }
-
-    }
-    fpr=fopen("data.dat","rb");
-    system("cls");
-    welcome();
-    char another='y';
-    while(1){
-        printf("\n\n\tEnter Book Name:");
-        fflush(stdin);
-        gets(b.name);
-        printf("\n%s\tEnter writer's Name:",b.name);
-        gets(b.writer);
-        printf("\n\tEnter Department Name:");
-        gets(b.department);
-        printf("\n\tEnter Self Number:");
-        fflush(stdin);
-        scanf("%s",b.self);
-
-        while(fread(&b,sizeof(b),1,fpr)){}
-            while(serial<=b.serial){
-                if(serial==b.serial){
-                    b.serial++;
-                    break;
-                }
-                serial++;
-            }
-
-        printf("\n%s\t Do you want to add another book?(y/n)",b.self);
-        fflush(stdin);
-        another=getch();
-        fwrite(&b,sizeof(b),1,fp);
-        if(another!='y' && another!='Y'){
-            fclose(fp);
-            fclose(fpr);
+            printf("\n\n\n System error ! ! \a\a\a");
             menu();
         }
-        else{
-            fclose(fp);
-            fclose(fpr);
-            add();
-        }
     }
+    fpr=fopen("data.dat","rb");
+
+    char another='y';
+
+    while(another=='y'){
 
 
+        fflush(stdin);
+        printf("\n\n\tEnter book name:");
+        gets(b.name);
+        strupr(b.name);
 
+        printf("\n\tEnter Writers name:");
+        gets(b.writer);
+        printf("\n\tEnter department name:");
+        gets(b.department);
+        printf("\n\tEnter Book-self Number:");
+        gets(b.self);
+        printf("\n\tHow many Books:");
+        gets(b.number);
+
+        fwrite(&b,sizeof(b),1,fp);
+
+        printf("\n\\tDo you want to add another book?: [ y / n ]");
+        another=getch();
+
+    }
+    fclose(fp);
+    menu();
 }
 
 void list(){
     FILE *fpr;
-    int i=6;
+    int i=6,serial=1;
     fpr=fopen("data.dat","rb");
     system("cls");
     welcome();
-    printf("\n\n\nSerial\tName\t\tWriter\t\t Self number\n");
+    printf("\n\n\nSerial\tName\t\t\tWriter\t\t\tDepartment\tSelf number\tTotal Number of books");
     while(fread(&b,sizeof(b),1,fpr)){
             gotoxy(2,i);
-            printf("%d",b.serial);
+            printf("%d",serial);
             gotoxy(8,i);
             printf("%s",b.name);
-            gotoxy(24,i);
+            gotoxy(32,i);
             printf("%s",b.writer);
-            gotoxy(42,i);
-            printf("%d",b.self);
+            gotoxy(56,i);
+            printf("%s",b.department);
+            gotoxy(77,i);
+            printf("%s",b.self);
+            gotoxy(92,i);
+            printf("%s",b.number);
             i+=2;
+            serial++;
     }
     fclose(fpr);
-    printf("\n\n\n\t\tPress any key to.......");
-    getch();
-    menu();
-
+    char choice;
+    printf("\n\n\n\t.....Press ESC to exit............");
+    do{
+        choice=getch();
+        if(choice == 27)
+            menu();
+    }while(1);
 
 }
 
 void search(){
+    FILE *fpr;
+    fpr=fopen("data.dat","rb");
+    char name[40];
+    int found=0,serial=1,i=8;
+    system("cls");
+    welcome();
+    fflush(stdin);
+    printf("\n\t Enter Book Name:");
+    gets(name);
+    strupr(name);
+    printf("\n\n\nSerial\tName\t\t\tWriter\t\t\tDepartment\tSelf number\tTotal Number of books");
+    while(fread(&b,sizeof(b),1,fpr)){
 
+        if(!strcmp(b.name,name)){
+
+            gotoxy(2,i);
+            printf("%d",serial);
+            gotoxy(8,i);
+            printf("%s",b.name);
+            gotoxy(32,i);
+            printf("%s",b.writer);
+            gotoxy(56,i);
+            printf("%s",b.department);
+            gotoxy(77,i);
+            printf("%s",b.self);
+            gotoxy(92,i);
+            printf("%s",b.number);
+            i+=2;
+            serial++;
+
+
+
+            found=1;
+        }
+
+    }
+    if(!found)
+        printf("\n\n\tBook not found.... \a\a");
+
+    fclose(fpr);
+    char choice;
+    printf("\n\n\n\t.....Press ESC to exit............");
+    do{
+        choice=getch();
+        if(choice == 27)
+            menu();
+    }while(1);
 }
 
-void modify(){
 
+
+void modify(){
+    system("cls");
+    welcome();
+    FILE *fp;
+    fp=fopen("data.dat","rb+");
+    struct books s;
+
+    char name[40];
+    char choice;
+
+    printf("\n\tEnter Name to modify:");
+    fflush(stdin);
+    gets(name);
+    strupr(name);
+    while(fread(&b,sizeof(b),1,fp)){
+
+        if(!strcmp(b.name,name)){
+
+            fflush(stdin);
+            printf("\n\n\tEnter New book name:");
+            gets(s.name);
+            strupr(s.name);
+            printf("\n\tEnter New Writers name:");
+            gets(s.writer);
+            printf("\n\tEnter New department name:");
+            gets(s.department);
+            printf("\n\tEnter New Book-self Number:");
+            gets(s.self);
+            printf("\n\tHow many Books(updated):");
+            gets(s.number);
+            fseek(fp,-sizeof(b),SEEK_CUR);
+            fwrite(&s,sizeof(b),1,fp);
+            break;
+            }
+            else{
+                printf("\n\n\tNo book found ...\n\n\t1.Retry \t\t||\t  Or press \' ESC \' to exit...");
+                do{
+                    choice=getch();
+                    if(choice==27)
+                        menu();
+                    else if(choice=='1')
+                        modify();
+                }while(1);
+            }
+    }
+
+
+
+    fclose(fp);
+
+    printf("\n\n\n\t.....Press ESC to exit............");
+    do{
+        choice=getch();
+        if(choice == 27)
+            menu();
+    }while(1);
 }
 
 void removes(){
+    system("cls");
+    welcome();
+    FILE *fpr,*temp;
+    int flag=0;
+    char name[40];
+    fpr=fopen("data.dat","rb");
+    if(!fpr){
+        printf("\n\n\n\t  System Error !! \a\a\a");
+    }
+    else{
+
+        temp=fopen("temp","wb+");
+        if(!temp){
+            printf("\n\n\system error press any key");
+            getch();
+            menu();
+        }
+        printf("\n\tEnter Book Name:");
+        fflush(stdin);
+        gets(name);
+        strupr(name);
+
+        while(fread(&b,sizeof(b),1,fpr)){
+            if(strcmp(name,b.name)){
+                fwrite(&b,sizeof(b),1,temp);
+            }
+            if(!strcmp(name,b.name))
+                flag=1;
+
+        }
+
+        fclose(fpr);
+        fclose(temp);
+
+        if(!flag)
+	{
+		printf("\n\n\t NO BOOK TO DELETE.");
+		remove("temp");
+	}
+		else
+		{
+			remove("data.dat");
+			rename("temp","data.dat");
+			printf("RECORD DELETED SUCCESSFULLY.");
+
+		}
+
+    }
+    char choice;
+    printf("\n\n\n\t.....Press ESC to exit............");
+    do{
+        choice=getch();
+        if(choice == 27)
+            menu();
+    }while(1);
 
 }
